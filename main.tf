b550cd09797b8de9de7576d81d4ed798d22d0a8b
gurusamyganesh <ganeshgurusamy@outlook.com>
2023-09-06 14:14:48 -0500

    actions file

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-east-2"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "harc-redshift-testdata"
+    key            = "terraformstate/terraform.tfstate"
+    region         = "us-east-2"
+  }
+}
+
 resource "aws_vpc" "main" {
  cidr_block = "10.0.0.0/16"
  
