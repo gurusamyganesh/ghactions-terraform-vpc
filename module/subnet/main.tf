b34439026c13081a56663f724c853d28700c6942
gurusamyganesh <ganeshgurusamy@outlook.com>
2023-09-26 14:24:03 -0500

    added subnet

---
+++ module/subnet/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket         = "harc-redshift-testdata"
+    key            = "terraformstate/terraform-subnet.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = var.vpc_id_for_subnet
+  cidr_block = "10.0.1.0/24"
+  map_customer_owned_ip_on_launch = "false"
+  availability_zone = "eu-west-2a"
+  tags {
+    name = "subnet for gitactions"
+  }
+}
+
+output "subnet_id123" {
+  value = aws_subnet.private_subnet.id
+}
