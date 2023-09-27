3349fef01b4ea8e0418573616dd686a472e5ebac
gurusamyganesh <ganeshgurusamy@outlook.com>
2023-09-27 13:44:11 -0500

    added subnet

--- module/subnet/variables.tf
+++ module/subnet/variables.tf
@@ -2,6 +2,11 @@ variable "vpc_id_for_subnet" {
     type = string
 }
 
+variable "vpc_id_for_subnet" {
+  type = string
+  default = "vpc-071eeee235e4605a9"
+}
+
 variable "user_names" {
   type    = list(string)
   default = ["user1", "user2", "user3"]
